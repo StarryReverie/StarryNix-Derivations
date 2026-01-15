0ea093e413756055a0137adb4f0a53202a8b9a0e
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-15 16:12:42 +0800

    feat(pkgs/by-name/lx-music): drop

--- pkgs/by-name/package-set.nix
+++ pkgs/by-name/package-set.nix
@@ -1,7 +1,5 @@
 callPackage: {
 	cavasik = callPackage ./cavasik/package.nix { };
 
-	lx-music = callPackage ./lx-music/package.nix { };
-
 	orchis-kde = callPackage ./orchis-kde/package.nix { };
 }
