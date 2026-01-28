c05784eff43775dff0a161646f24bd3c95e35629
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-28 09:44:26 +0800

    feat(pkgs/by-name/kvlibadwaita): init at 0-unstable-2025-09-13

---
+++ pkgs/by-name/kvlibadwaita/package.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "kvlibadwaita";
+  version = "0-unstable-2025-09-13";
+
+  src = fetchFromGitHub {
+    owner = "GabePoel";
+    repo = "KvLibadwaita";
+    rev = "1f4e0bec44b13dabfa1fe4047aa8eeaccf2f3557";
+    hash = "sha256-jCXME6mpqqWd7gWReT04a//2O83VQcOaqIIXa+Frntc=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/share/Kvantum
+    cp -r src/KvLibadwaita $out/share/Kvantum/
+  '';
+
+  meta = {
+    description = "Libadwaita style theme for Kvantum. Based on Colloid-kde.";
+    homepage = "https://github.com/GabePoel/KvLibadwaita";
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ starryreverie ];
+  };
+}
--- pkgs/by-name/package-set.nix
+++ pkgs/by-name/package-set.nix
@@ -1,5 +1,7 @@
 callPackage: {
 	cavasik = callPackage ./cavasik/package.nix { };
 
+	kvlibadwaita = callPackage ./kvlibadwaita/package.nix { };
+
 	orchis-kde = callPackage ./orchis-kde/package.nix { };
 }
